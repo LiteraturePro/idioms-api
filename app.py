18162dd18aa86357b4311d909452d538090d1af6
literature <65801567+LiteraturePro@users.noreply.github.com>
2022-03-18 20:44:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,50 @@ def db():
         cnx.close()
         return jsonify(err)
     
-@app.route('/api')
+@app.route("/api")
 def api():
-    return str(datetime.now())
+    config = {
+        'url': 'https://sts.tencentcloudapi.com/',
+        # 域名，非必须，默认为 sts.tencentcloudapi.com
+        'domain': 'sts.tencentcloudapi.com', 
+        # 临时密钥有效时长，单位是秒
+        'duration_seconds': 1800,
+        'secret_id': os.environ['COS_SECRET_ID'],
+        # 固定密钥
+        'secret_key': os.environ['COS_SECRET_KEY'],
+        # 设置网络代理
+        # 'proxy': {
+        #     'http': 'xx',
+        #     'https': 'xx'
+        # },
+        # 换成你的 bucket
+        'bucket': 'cloud-1258693536',
+        # 换成 bucket 所在地区
+        'region': 'ap-shanghai',
+        # 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径
+        # 例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+        'allow_prefix': 'exampleobject', 
+        # 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+        'allow_actions': [
+            # 简单上传
+            'name/cos:PutObject',
+            'name/cos:PostObject',
+            # 分片上传
+            'name/cos:InitiateMultipartUpload',
+            'name/cos:ListMultipartUploads',
+            'name/cos:ListParts',
+            'name/cos:UploadPart',
+            'name/cos:CompleteMultipartUpload'
+        ],
+
+    }
+
+    try:
+        sts = Sts(config)
+        response = sts.get_credential()
+        return json.dumps(dict(response), indent=4)
+    except Exception as e:
+        return str(e)
 
 @app.route('/version',methods=['POST','GET'])
 def version():
